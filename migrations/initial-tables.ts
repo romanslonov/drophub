9f99fa8fccd6272549d3a8f6d8cf5a4149953780
Roman Slonov <destabiliseus@gmail.com>
2022-01-07 22:44:47 +0800

    Fixed migration

--- migrations/initial-tables.ts
+++ migrations/initial-tables.ts
@@ -5,7 +5,7 @@ export async function up(knex: Knex) {
     table.increments('id').primary();
     table.string('email', 255).unique().index();
     table.string('name', 255).notNullable();
-    table.integer('usage').notNullable();
+    table.integer('usage').notNullable().defaultTo(0);
     table.string('password', 255).notNullable();
   });
 
